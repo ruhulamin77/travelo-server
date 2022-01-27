ba99ca2926c9ccd8075c5f4111a5a4ad8a803217
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2022-01-27 14:19:22 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
+const app = express();
+// const port = 5000;
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qu1uq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("travelo");
+    const blogsCollection = database.collection("blogs");
+    const usersCollection = database.collection("users");
+
+    // POST API
+    app.post("/blogs", async (req, res) => {
+      const blog = req.body;
+      const result = await blogsCollection.insertOne(blog);
+      res.json(result);
+    });
+
+    // GET API
+    app.get("/blogs", async (req, res) => {
+      const cursor = blogsCollection.find({});
+      const blog = await cursor.toArray();
+      res.json(blog);
+    });
+    // get single blog
+    app.get("/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const blog = await blogsCollection.findOne(query);
+      res.json(blog);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Travelo!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
